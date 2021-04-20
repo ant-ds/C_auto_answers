ea301d1f78d651a7f54656408b984d2d9087fbad
ant-ds <antoine00@live.be>
2021-04-20 12:56:29 +0200

    Add runall functionality

--- scripts/compile_run_all.py
+++ scripts/compile_run_all.py
@@ -4,21 +4,25 @@ from os import path
 import subprocess
 from tqdm import tqdm
 
+####
+#  File find helper functions
+####
+
 
 def get_c_files(path):
     """
-    Return a list of all c files in format (complete path, filename)
+    Return a list of all c files (complete path)
     """
     # Get all files in path
     all_files = []
     for root, dirs, files in os.walk(path, topdown=True):
         for name in files:
-            all_files.append((os.path.join(root, name), name))
+            all_files.append(os.path.join(root, name))
 
     # Keep only files ending in .c
     c_files = []
     for files in all_files:
-        if files[0][-2:].lower() == ".c":
+        if files[-2:].lower() == ".c":
             c_files.append(files)
     # TODO: add skip files in ignorelist (that don't need to be compiled)
 
@@ -26,7 +30,7 @@ def get_c_files(path):
     return(c_files)
 
 
-def get_all_directories(path):
+def get_directories(path):
     """
     Returns a list of all directories in given path
     """
@@ -38,16 +42,36 @@ def get_all_directories(path):
     return all_dirs
 
 
-def compile_files(file_tups):
+def get_out_files(path):
     """
-    Compiles all given files in destination path
-    file_tups are tuples of format: (complete path, filename)
-    Returns list of all output paths of .out files
+    Return a list of all out files (complete path, filename)
+    """
+    # Get all files in path
+    all_files = []
+    for root, dirs, files in os.walk(path, topdown=True):
+        for name in files:
+            all_files.append(os.path.join(root, name))
+
+    # Keep only files ending in .c
+    c_files = []
+    for files in all_files:
+        if files[-4:].lower() == ".out":
+            c_files.append(files)
+    # TODO: add skip files in ignorelist (that don't need to be compiled)
+
+    # Return list of tuples: (complete path, filename)
+    return(c_files)
+
+
+def compile_files(c_files):
+    """
+    Compiles all given c_files
+    Returns list of all .out paths
     """
     all_output_paths = []
-    for path, name in tqdm(file_tups, 'Compling...'):
+    for path in tqdm(c_files, "Compiling..."):
         # Create output path
-        output_path = path[:-2] + '.out'
+        output_path = path[:-2] + ".out"
 
         # Compile to output
         proc = subprocess.run(
@@ -62,13 +86,20 @@ def run_all(paths):
     """
     Runs all .out files specified in the list of paths
     """
-    # TODO
-    pass
+    for path in tqdm(paths, "Running: "):
+        # Check existence of .input file
+        input_file = path[:-4] + ".input"
+        assert os.path.isfile(
+            input_file), f"{input_file} doesn't exist!\nPlease create this input file"
+        proc = subprocess.run(
+            f"./{path} < {input_file}".split(), capture_output=True)
 
 
 def main():
-    file_tups = get_c_files(path='.')
-    out_files = compile_files(file_tups=file_tups)
+    run_path = '.'
+    c_files = get_c_files(path=run_path)
+    out_files = compile_files(c_files=c_files)
+    # out_files = get_out_files(path=run_path)
     run_all(paths=out_files)
 
 
